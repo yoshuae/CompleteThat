a5be2c202011a8b8b87cfda1cec84e9ff2229e71
yoshuae <jae83@cornell.edu>
2015-01-02 15:06:13 -0600

    formated __init__

--- completethat/__init__.py
+++ completethat/__init__.py
@@ -10,19 +10,19 @@ Frobenius norm of the objective matrix) of the following problem:
 Where, M represents the data matrix and \Omega represents the set of p observed entries of M
 
     Usage:
-	# MatrixCompletion
+    # MatrixCompletion
     >>> from completethat import MatrixCompletion
     >>> problem = MatrixCompletion(M) 
     >>> problem.complete_it(algo_name) 
     >>> X = problem.get_matrix() 
     >>> out_info = problem.get_out() #Extra info (iterations, ect)
 
-	# MatrixCompletionBD
-	>>>from matrix_completion_BD import MatrixCompletionBD
-	>>>temp=MatrixCompletionBD('input_data.txt')
-	>>>temp.train_sgd(dimension=6,init_step_size=.01,min_step=.000001, reltol=.001,rand_init_scale=10, maxiter=1000,batch_size_sgd=50000,shuffle=True):
-	>>>temp.validate_sgd('test_data.txt')
-	>>>temp.save_model()
+    # MatrixCompletionBD
+    >>>from matrix_completion_BD import MatrixCompletionBD 
+    >>>temp=MatrixCompletionBD('input_data.txt')
+    >>>temp.train_sgd(dimension=6,init_step_size=.01,min_step=.000001, reltol=.001,rand_init_scale=10,   maxiter=1000,batch_size_sgd=50000,shuffle=True):
+    >>>temp.validate_sgd('test_data.txt')
+    >>>temp.save_model()
 
 """
 from matrix_completion import MatrixCompletion
